6681399200e8e8838d2d9e83d707ca50a0af9550
jaleman <john.aleman@cyphyworks.com>
2015-09-10 21:28:03 -0400

    First pass at the Database Class

--- CentralSystem/src/LazIR-Brain-v1.0.cpp
+++ CentralSystem/src/LazIR-Brain-v1.0.cpp
@@ -1,39 +1,99 @@
 //============================================================================
-// Name        : 0.cpp
+// Name        : LazIR-Brain.cpp
 // Author      : John R. Aleman
-// Version     :
+// Version     : 1.0
 // Copyright   : Mwhaha
-// Description : Hello World in C++, Ansi-style
+// Description : This is the main loop of the LazIR server
 //============================================================================
 
-#include <iostream>
-#include <list>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <netdb.h>
 #include "Definitions.h"
 #include "Functions.h"
 #include "LazIRDatabase.h"
 using namespace std;
 
+int main(int argc, char *argv[]) {
+	//Here we configure the network settings
+	int sockfd;
+	struct addrinfo servHints, *servinfo, *p;
+	int rv;
+	int numbytes;
+	struct sockaddr_storage device_addr;
+	char buf[MAXBUFLEN];
+	socklen_t addr_len;
+	char s[INET6_ADDRSTRLEN];
+	memset(&servHints, 0, sizeof servHints);
+	servHints.ai_family = AF_UNSPEC; // set to AF_INET to force IPv4
+	servHints.ai_socktype = SOCK_DGRAM;
+	servHints.ai_flags = AI_PASSIVE; // use my IP
+	if ((rv = getaddrinfo(NULL, MYPORT, &servHints, &servinfo)) != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+		return 1;
+	}
+	// loop through all the results and bind to the first we can
+	for (p = servinfo; p != NULL; p = p->ai_next) {
+		if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol))
+				== -1) {
+			perror("listener: socket");
+			continue;
+		}
+		fcntl(sockfd, F_SETFL, O_NONBLOCK);
+		if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+			close(sockfd);
+			perror("listener: bind");
+			continue;
+		}
+		break;
+	}
+	if (p == NULL) {
+		fprintf(stderr, "listener: failed to bind socket\n");
+		return 2;
+	}
+	freeaddrinfo(servinfo);
 
-int main() {
-	LazIR_Database BaseStations(BASESTATION_MIN_ID, BASESTATION_MAX_ID);
-	LazIR_Database Players(PLAYER_MIN_ID, PLAYER_MIN_ID);
-	LazIR_Database Others(OTHER_MIN_ID, OTHER_MIN_ID);
+
+
+
+	int gameMode = 1;
+	switch (gameMode) {
+	case 0: //Preliminary Mode
+		//Do Stuff Here
+		break;
+	case 1: //Game main loop
+		//Do Stuff Here
+		break;
+	case 2: //What happens at the end of the game
+		//Do Stuff Here
+		break;
+	default:
+		cout << "Error: An unknown value for gameMode was encountered" << endl;
+	}
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	int a, b, c, d, e, f, g;
+	Players.addDevice(21);
+	Players.addDevice(22);
+	Players.addDevice(23);
+	Players.addDevice(24);
+	bool mau = Players.deviceExists(21);
+	Players.writeValue(23, batteryLevel, 40);
+	b = Players.numberOfDevices();
+	cout << "number of devices: " << b << endl;
+	vector<int> something;
+	c = Players.devicesWithLowBattery(something);
+	cout << "cc: " << c << endl;
+	for (int i = 0; i < something.size(); i++) {
+		cout << "elements are << " << something.at(i) << " " << endl;
+	}
+	a = Players.deviceExists(21);
+	b = Players.numberOfDevices();
+	cout << "cc: " << c << endl;
+	cout << "bb: " << b << endl;
+	Players.printDeviceInfo(10);
 	//int foo = SUPERTEST;
+
+
+	close(sockfd);
 	return 0;
 }
-
-
 
 /*
  *
--- CentralSystem/src/LazIRDatabase.cpp
+++ CentralSystem/src/LazIRDatabase.cpp
@@ -15,55 +15,160 @@ LazIR_Database::LazIR_Database(int min, int max) {
 
 //Public Functions
 
-bool LazIR_Database::LazIR_Database::deviceExists(int deviceID) {
-	bool result = true;
+bool LazIR_Database::LazIR_Database::deviceExists(uint8_t deviceID) {
+	bool result = false;
+	std::vector<std::vector<uint8_t> >::iterator devicesIterator;
+	int a = this->DeviceDatabase.size();
+	if (a == 0) {
+		result = false;
+	} else {
+		for (devicesIterator = DeviceDatabase.begin();
+				devicesIterator != DeviceDatabase.end(); ++devicesIterator) {
+			if (devicesIterator->at(0) == deviceID) {
+				result = true;
+			}
+		}
+	}
 	return result;
 }
-int LazIR_Database::addDevice(int deviceID) {
+int LazIR_Database::addDevice(uint8_t deviceID) {
 	int error = 0;
+	if (deviceExists(deviceID)) {
+		error = 1;
+		std::cout << "Error " << error
+				<< " while adding new device to the database, device already exists."
+				<< std::endl;
+	} else {
+		std::vector<uint8_t> newDevice;
+		newDevice.push_back(deviceID);
+		newDevice.push_back(0);
+		newDevice.push_back(0);
+		newDevice.push_back(0);
+		newDevice.push_back(0);
+		newDevice.push_back(0);
+		newDevice.push_back(0);
+		newDevice.push_back(0);
+		newDevice.push_back(0);
+		newDevice.push_back(0);
+		newDevice.push_back(0);
+		newDevice.push_back(0);
+		DeviceDatabase.push_back(newDevice);
+		error = 0;
+	}
 	return error;
 }
-int LazIR_Database::removeDevice(int deviceId) {
-	int error = 0;
+int LazIR_Database::removeDevice(uint8_t deviceID) {
+	int error = -1;
+	std::vector<std::vector<uint8_t> >::iterator devicesIterator;
+	for (devicesIterator = DeviceDatabase.begin();
+			devicesIterator != DeviceDatabase.end(); ++devicesIterator) {
+		if (devicesIterator->at(0) == deviceID) {
+			DeviceDatabase.erase(devicesIterator);
+			error = 0;
+			break;
+		}
+	}
 	return error;
 }
 //These functions provide information about the database
-//int LazIR_Database::fetchDeviceIDList(deviceIDs result) {int
-//	int error;
-//	return error;
-//}
+int LazIR_Database::fetchDeviceIDList(std::vector<int> &deviceIDs) {
+	int error = 0;
+	std::vector<std::vector<uint8_t> >::iterator devicesIterator;
+	if (DeviceDatabase.size() == 0) {
+		error = -1;
+	} else {
+		for (devicesIterator = DeviceDatabase.begin();
+				devicesIterator != DeviceDatabase.end(); ++devicesIterator) {
+			deviceIDs.push_back(devicesIterator->at(id));
+		}
+	}
+	error = DeviceDatabase.size();
+	return error;
+}
 int LazIR_Database::numberOfDevices() {
-	int result = 0;
+	int result = DeviceDatabase.size();
 	return result;
 }
 int LazIR_Database::maxValue(int field) {
 	int result = 0;
+	std::vector<std::vector<uint8_t> >::iterator devicesIterator;
+	for (devicesIterator = DeviceDatabase.begin();
+			devicesIterator != DeviceDatabase.end(); ++devicesIterator) {
+		if (devicesIterator->at(field) > result) {
+			result = devicesIterator->at(field);
+		}
+	}
 	return result;
 }
 int LazIR_Database::minValue(int field) {
-	int result = 0;
+	int result = 0xFF;
+	std::vector<std::vector<uint8_t> >::iterator devicesIterator;
+	for (devicesIterator = DeviceDatabase.begin();
+			devicesIterator != DeviceDatabase.end(); ++devicesIterator) {
+		if (devicesIterator->at(field) < result) {
+			result = devicesIterator->at(field);
+		}
+	}
 	return result;
 }
-//deviceIDs LazIR_Database::devicesWithLowBattery(deviceIDs result) {
-//	int error;
-//	return error;
-//}
-int LazIR_Database::printDeviceInfo(int deviceID) {
+int LazIR_Database::devicesWithLowBattery(std::vector<int> &deviceIDs) {
 	int error = 0;
+	std::vector<std::vector<uint8_t> >::iterator devicesIterator;
+	if (this->DeviceDatabase.size() == 0) {
+		error = -1;
+	} else {
+		for (devicesIterator = DeviceDatabase.begin();
+				devicesIterator != DeviceDatabase.end(); ++devicesIterator) {
+			if (devicesIterator->at(batteryLevel) < 20) {
+				deviceIDs.push_back(devicesIterator->at(id));
+				error++;
+			}
+		}
+	}
 	return error;
 }
-uint8_t LazIR_Database::readValue(int deviceID, int field) {
-	int result = 0;
+int LazIR_Database::printDeviceInfo(uint8_t deviceID) { ///This functions is still not made
+	int error = 0;
+	std::vector<std::vector<uint8_t> >::iterator devicesIterator;
+	std::vector<uint8_t>::iterator dataIterator;
+	std::cout << "Database   " << "mau" << std::endl;
+	for (devicesIterator = DeviceDatabase.begin();
+			devicesIterator != DeviceDatabase.end(); ++devicesIterator) {
+		for (dataIterator = devicesIterator->begin();
+				dataIterator != devicesIterator->end(); ++dataIterator){
+			std::cout << std::setw(5) << int(*dataIterator);
+		}
+		std::cout << std::endl;
+	}
+	return error;
+}
+uint8_t LazIR_Database::readValue(uint8_t deviceID, int field) {
+	int result = 0xFF;
+	std::vector<std::vector<uint8_t> >::iterator devicesIterator;
+	for (devicesIterator = DeviceDatabase.begin();
+			devicesIterator != DeviceDatabase.end(); ++devicesIterator) {
+		if (devicesIterator->at(0) == deviceID) {
+			result = devicesIterator->at(field);
+		}
+	}
 	return result;
 }
 
-int LazIR_Database::writeValue(int deviceID, int field, uint8_t newValue) {
+int LazIR_Database::writeValue(uint8_t deviceID, int field, uint8_t newValue) {
 	int error = 0;
+	std::vector<std::vector<uint8_t> >::iterator devicesIterator;
+	for (devicesIterator = DeviceDatabase.begin();
+			devicesIterator != DeviceDatabase.end(); ++devicesIterator) {
+		if (devicesIterator->at(0) == deviceID) {
+			devicesIterator->at(field) = newValue;
+			return error;
+		}
+	}
+	error = -1;
 	return error;
 }
-
 
 LazIR_Database::~LazIR_Database() {
-	// TODO Auto-generated destructor stub
+// TODO Auto-generated destructor stub
 }
 
